6394a0cdd9c422fd7022b17f509c3cebf21aab55
WakeDown <wakedown.59@gmail.com>
2015-10-08 10:15:27 +0500

    1

--- DataProvider/StuffDb/StoredProc/RestHoliday/rest_holiday_get.sql
+++ DataProvider/StuffDb/StoredProc/RestHoliday/rest_holiday_get.sql
@@ -1,15 +1,10 @@
-﻿CREATE PROCEDURE [dbo].[rest_holiday_list]
-	@employee_sid varchar(46) = null,
-	@year int
+﻿CREATE PROCEDURE [dbo].[rest_holiday_get]
+	@id int
 AS
 	begin
 	set nocount on;
-
 	select rh.id, rh.start_date, rh.end_date, rh.duration, case when can_edit=1 and confirmed=0 then 1 else 0 end as can_edit,case when confirmed=1 then 1 else 0 end as confirmed, employee_sid, e.display_name as employee_name
 	from rest_holidays rh
 	inner join employees e on rh.employee_sid = e.ad_sid
-	where rh.enabled=1
-	order by employee_sid, start_date
-	
-
+	where rh.id=@id
 	end
